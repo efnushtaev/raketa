57b5a2e1b5b380812482c0ac5e439694d61dd0dd
efnushtaev <efnushtaev@gmail.com>
2020-07-01 11:32:51 +0300

    Updates

---
+++ precache-manifest.5437ed734ef32bcfa37922b9a26eefd4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efde5f167f0e14afac13c619faf77092",
+    "url": "/raketa/index.html"
+  },
+  {
+    "revision": "b09f2bf7d00335fa1771",
+    "url": "/raketa/static/js/2.c0e291d7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/raketa/static/js/2.c0e291d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05b1ef66bbff48a5a17c",
+    "url": "/raketa/static/js/main.f1705a16.chunk.js"
+  },
+  {
+    "revision": "f0e5a5c833bee7c5e497",
+    "url": "/raketa/static/js/runtime-main.13b6a5bd.js"
+  }
+]);
